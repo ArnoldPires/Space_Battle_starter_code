4a4b2e7979b40e965c778312e0a2a654e5ff5e60
ArnoldPires <arnold.pires92@gmail.com>
2023-08-04 10:24:46 -0400

    Just some playership code

--- app.js
+++ app.js
@@ -19,17 +19,50 @@ A game round would look like this:
 * You win the game if you destroy all of the aliens
 * You lose the game if you are destroyed
 */
-
 // Player Ship Properties:
 /*
   * USS Sarah has 20 hitpoints total if reachs 0, game over.
   * Player ship does 5 hitpoint damage if it hits.
   * Player ship has a .7 chance of hitting the enemy ship so Math.random needs to be used.
 */
-
 // Alien Ship Properties:
 /*
   * Alien ship has between 3 to 6 hitpoints.
   * Alien ship can do between 2 to 4 damage.
   * Alien ship has an accuracy between .6 to .8
 */
+
+// Creating both the player and enemy ships
+class Ships {
+  constructor(name, hull, firepower, accuracy) {
+    this.name = name;
+    this.hull = hull;
+    this.firepower = firepower;
+    this.accuracy = accuracy;
+  }
+}
+
+// Define player ship
+const playerShip = new Ships("USS Sarah", 20, 5, 0.7);
+
+// Define the 6 Alien ships
+const alienShips = [];
+for (let i = 1; i <= 6; i++) {
+  const hull = randomNum(3, 6);
+  const firepower = randomNum(2, 4);
+  const accuracy = (randomNum(6, 8) / 10).toFixed(1);
+  const alienShip = new Ships(`Alien Ship ${i}`, hull, firepower, accuracy);
+  alienShips.push(alienShip);
+}
+
+// Define randomNum so the above code can work with the alien ships
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// A function that will determine if an attack is successful or not based on the ship's accuracy
+this.goodHit = function () {
+    return Math.random() < this.accuracy;
+  }
+
+// Method used to attack the other ship
